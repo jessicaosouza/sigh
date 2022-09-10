42e5f9b12407179370fb70c3d965cf22fad33d6d
Jessica Souza <jessica@gmail.com>
2022-09-10 11:35:36 -0300

    test: testando sincronizacao git

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -8,11 +8,6 @@ use App\Models\Role;
 
 class RoleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
